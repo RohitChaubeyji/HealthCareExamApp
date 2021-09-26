39eb58edc420cf8012957dba3d76477a1d313678
Rohit Kumar <rkc2772@gmail.com>
2021-09-26 21:01:57 +0530

    first commit

---
+++ src/main/java/in/nareshit/raghu/HealthCareExamAppApplication.java
@@ -0,0 +1,13 @@
+package in.nareshit.raghu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareExamAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCareExamAppApplication.class, args);
+	}
+
+}
